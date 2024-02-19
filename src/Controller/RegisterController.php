e910cd39f7151bc6f03467af909dea55b5ffabc4
Antoine Despres <antoinedespres13@gmail.com>
2024-02-19 14:57:19 +0100

    :sparkles: Login / Register with API Platform

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AuthUser;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+    private $passwordHasher;
+    private $JWTManager;
+    private $validator;
+
+    public function __construct(
+        EntityManagerInterface $entityManager, 
+        UserPasswordHasherInterface $passwordHasher, 
+        JWTTokenManagerInterface $JWTManager,
+        ValidatorInterface $validator
+    ) {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+        $this->JWTManager = $JWTManager;
+        $this->validator = $validator;
+    }
+    #[Route("/api/register", name: "register", methods: ["POST"])]
+    public function register(Request $request , SerializerInterface $serializer)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if ( empty($data['username']) || empty($data['password']) ) {
+            return new JsonResponse(['error' => 'Expecting mandatory parameters!']);
+        }
+
+        $user = new AuthUser();
+        $user->setUsername($data['username']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $data['password']));
+        $user->setRoles(['ROLE_USER']);
+        $validationErrors = $this->validator->validate($user);
+        if (count($validationErrors) > 0) {
+            throw new \Exception((string) $validationErrors[0]);
+        }
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $token = $this->JWTManager->create($user);
+        $user = $serializer->serialize([
+            'user' => $user,
+            'token' => $token
+        ], 'json', ['groups' => 'user:deep']);
+        return new JsonResponse( $user , 200 , [] , true);
+    }
+}
--- src/Entity/AuthUser.php
+++ src/Entity/AuthUser.php
@@ -5,35 +5,63 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AuthUserRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 #[ORM\Entity(repositoryClass: AuthUserRepository::class)]
 #[ApiResource]
-class AuthUser
+class AuthUser implements UserInterface,PasswordAuthenticatedUserInterface
 {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer', unique: true, nullable: false)]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255 , unique: true , nullable: false)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    #[Groups(['user:deep'])]
+    private ?string $username = null;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
     #[ORM\Column(length: 255)]
-    private ?string $authname = null;
-
-    #[ORM\Column(length: 2500)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 8, 
+        max: 255 , 
+        minMessage: 'Your password must be at least {{ limit }} characters long',
+        maxMessage: 'Your password cannot be longer than {{ limit }} characters',
+    )] 
+    // #[CustomAssert\ContainsUserPassword]
     private ?string $password = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getAuthname(): ?string
+    public function getUsername(): ?string
     {
-        return $this->authname;
+        return $this->username;
     }
 
-    public function setAuthname(string $authname): static
+    public function setUsername(string $username): static
     {
-        $this->authname = $authname;
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
 
         return $this;
     }
@@ -49,4 +77,15 @@ class AuthUser
 
         return $this;
     }
+    public function eraseCredentials() : void
+    {
+        return ;
+    }
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
 }
+
+
